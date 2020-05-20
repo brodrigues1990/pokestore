b5f18b1026cb324fad8505b12d3756d7b85ae6d8
Bruno Rodrigues <brodrigues1990@gmail.com>
2020-05-20 05:38:44 -0300

    change header

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import { makeStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import { Container, Toolbar, InputBase } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Logo from '../assets/PokeStoreLogo.png'
+import SearchIcon from '@material-ui/icons/Search';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
         marginBottom: 35
     },
-    image: {
+    logoImage: {
         height: 70,
+    },
+    logoContainer: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
     },
 }));
 
@@ -20,13 +68,26 @@ const Header = (props) => {
     return (
         <>
             <AppBar position="static" className={classes.root}>
-                <Toolbar>
-                    <Grid container justify="center" >
-                        <Grid item >
-                            <img src={Logo} alt="PokeStore" className={classes.image} />
-                        </Grid>
-                    </Grid>
-                </Toolbar>
+                <Container fixed>
+                    <Toolbar>
+                        <div className={classes.logoContainer}>
+                            <img src={Logo} alt="PokeStore" className={classes.logoImage} />
+                        </div>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase
+                                placeholder="Search…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                                inputProps={{ 'aria-label': 'Caçar Pokémon...' }}
+                            />
+                        </div>
+                    </Toolbar>
+                </Container>
             </AppBar>
         </>
     );
