255e016212e70d751d6d37d0e5882087a0dfbff3
Bruno Rodrigues <brodrigues1990@gmail.com>
2020-05-27 13:19:57 -0300

    add template in error404 page

--- src/components/pages/error404.js
+++ src/components/pages/error404.js
@@ -3,12 +3,11 @@ import { useLocation, useHistory, Link } from 'react-router-dom';
 import {
     Paper,
     Grid,
-    Box,
-    CardMedia, 
 } from '@material-ui/core';
-import Error404Image from '../../assets/images/Error404.png'
-import { makeStyles } from '@material-ui/core/styles';
 import { Close as CloseIcon } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+import MarketTemplate from '../template/marketTemplate'
+import Error404Image from '../../assets/images/Error404.png'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -27,21 +26,23 @@ const Error404 = () => {
 
     return (
         <>
-            <Paper elevation={2} className={classes.root}>
-                <Grid container direction="column" justify="center" alignItems="center">
-                    <Grid item xs={12} >
-                        <img src={Error404Image} />
+            <MarketTemplate>
+                <Paper elevation={2} className={classes.root}>
+                    <Grid container direction="column" justify="center" alignItems="center">
+                        <Grid item xs={12} >
+                            <img src={Error404Image} />
+                        </Grid>
+                        <Grid item xs={12} >
+                            Nenhum resultado para <strong>{location.pathname}</strong>
+                        </Grid>
+                        <Grid item xs={12} >
+                            <Link onClick={e => history.goBack()}>
+                                <CloseIcon color="action" style={{ fontSize: 30 }} />
+                            </Link>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} >
-                        Nenhum resultado para <strong>{location.pathname}</strong>
-                    </Grid>
-                    <Grid item xs={12} >
-                        <Link onClick={e => history.goBack()}>
-                            <CloseIcon color="action" style={{ fontSize: 30 }} />
-                        </Link>
-                    </Grid>
-                </Grid>
-            </Paper>
+                </Paper>
+            </MarketTemplate>
         </>
     );
 }
